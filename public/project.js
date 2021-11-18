685d222da42593db738593f3623be33075a62f5a
Cole Tretbar <coletretbar@gmail.com>
2021-11-18 02:20:42 -0600

    All results now display on webpage with game logo and friends who own each game display, added css file

--- public/project.js
+++ public/project.js
@@ -10,10 +10,6 @@ async function addSteamId(){
     const response = await fetch(server_url);
     const json = await response.json();
 
-    //console.log(json);
-    //console.log(json.response.games);
-    //console.table(json.response.games);
-
     // Pushing the first list of games from the original user so it is the first index in the array
     combinedArray.push(json.response.games);
 
@@ -22,8 +18,6 @@ async function addSteamId(){
     const friendResponse = await fetch(friend_url);
     const friendData = await friendResponse.json();
 
-    //console.log(friendData);
-
     let friendSteamId = new Array();
 
     // Creating an array of only steam ids for the next request
@@ -31,43 +25,42 @@ async function addSteamId(){
         friendSteamId.push(friendData.friendslist.friends[i].steamid);
     }
 
-    //console.log(friendSteamId);
-
     // Using the friend list steam id list to request who is currently online
     const online_url = `/isOnlineApi/${friendSteamId}`;
     const onlineResponse = await fetch(online_url);
     const onlineData = await onlineResponse.json();
 
-    // console.log(onlineData.response);
-    //console.log(onlineData.response.players[0].personastate);
-
     let onlineStatus = new Array();
+    let onlineNameId = new Array();
+
     // This checks for which steam ids are currently online
     for(x = 0; x < onlineData.response.players.length; x++){
         if(onlineData.response.players[x].personastate == 1){
             onlineStatus.push(onlineData.response.players[x].steamid);
+            onlineNameId.push({friendid: onlineData.response.players[x].steamid, friendname: onlineData.response.players[x].personaname});
         }
     }
 
-    // console.log(onlineStatus);
+    var notPrivateList = new Array();
 
     // Loop that is getting the game list of each online steam id
     for(y = 0; y < onlineStatus.length; y++){
         const onlineGames_url = `/onlineGamesApi/${onlineStatus[y]}`;
         const onlineGames_response = await fetch(onlineGames_url);
         const friendGamesData = await onlineGames_response.json();
-        //console.log(friendGamesData);
-        let notPrivate = new Array();
+
         // Skips any steam id that is set to private which will not allow for retrieval of owned games
         if(friendGamesData.response.games != null){
-            // console.log(friendGamesData.response);
-            // console.table(friendGamesData.response.games);
             // Adding these new games lists to the original games list to make a single array of all responses
             combinedArray.push(friendGamesData.response.games);
+            notPrivateList.push(onlineNameId[y]);
+
+
         };
     }
 
-    //console.log(combinedArray);
+    // This adds a index[0] placeholder for one of the last steps of displaying which friends own the games
+    notPrivateList.unshift({friendid: '12345678945612547', friendname: "The Original User"})
 
     // This checks all arrays for occurrences of steam appids amongst all of the arrays
     // creates an occurrence for the original and all others for each unique appid
@@ -85,40 +78,45 @@ async function addSteamId(){
       
     combinedArray.forEach(arr => arr.forEach(obj => Object.assign(obj, result[obj.appid])));
 
-    // console.log(combinedArray);
-
     // Sorts the occurrences from greatest to least
-
     let finalArray = combinedArray[0].sort(
         (x, y) => y.occurrences - x.occurrences ||
         x.name.localeCompare(y.name));
-    
-    // console.log(finalArray);
-    // console.table(finalArray.where);
-
-    //console.table(combinedArray[0]);
-    //console.log(combinedArray[0].length);
-    //console.log(combinedArray[0][0].occurrences);
-
 
     // A loop for displaying all of the results into the console 
-    console.log("You have " + (finalArray[0].occurrences - 1) + " friend(s) online to play these game(s):");
-    count = 2;
-    for(k = 0; k < finalArray.length; k++){
-        b = k + 1;
-        if(finalArray[k].where[0] == 0){
-            if(finalArray[k].occurrences > 1){
-                if(finalArray[k].occurrences == finalArray[b].occurrences){
-                    console.log("       " + finalArray[k].name);
-                } else {
-                    console.log("       " + finalArray[k].name);
-                    if((finalArray[b].occurrences - 1) > 0){
-                    console.log("You have " + (finalArray[b].occurrences - 1) + " friend(s) online to play these game(s):");
-                    ++count;
-                    }
-                }
-        
-            }    
+    // console.log("You have " + (finalArray[0].occurrences - 1) + " friend(s) online to play these game(s):");
+    // count = 2;
+    // for(k = 0; k < finalArray.length; k++){
+    //     b = k + 1;
+    //     if(finalArray[k].where[0] == 0){
+    //         if(finalArray[k].occurrences > 1){
+    //             if(finalArray[k].occurrences == finalArray[b].occurrences){
+    //                 console.log("       " + finalArray[k].name);
+    //             } else {
+    //                 console.log("       " + finalArray[k].name);
+    //                 if((finalArray[b].occurrences - 1) > 0){
+    //                 console.log("You have " + (finalArray[b].occurrences - 1) + " friend(s) online to play these game(s):");
+    //                 ++count;
+    //                 }
+    //             }
+    //         }    
+    //     }
+    // }
+
+    for(p = 0; p < 10 ; p++){
+        var logoAppid = finalArray[p].appid;
+        var logoUrl = finalArray[p].img_logo_url;
+        var img = document.createElement("img");
+        img.src = "http://media.steampowered.com/steamcommunity/public/images/apps/" + logoAppid + "/" + logoUrl + ".jpg";
+        var src = document.getElementById("steamResults");
+        var friendText = "";
+        for(z = 1; z < finalArray[p].where.length; z++){ 
+            friendText = friendText + notPrivateList[finalArray[p].where[z]].friendname + ", ";
         }
-    } 
+        var resultPara = document.createElement("p");
+        var resultName = document.createTextNode(friendText);
+        resultPara.insertAdjacentElement("afterbegin", img);
+        resultPara.appendChild(resultName);
+        src.appendChild(resultPara);
+    }
 }
